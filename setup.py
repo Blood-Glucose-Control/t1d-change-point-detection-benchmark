e88f5ac6e77c43cf218d0b0c5d5a029a34e26a0c
Tony Chan <tony.chan@sciteline.com>
2024-12-11 03:07:05 -0500

    new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="t1d_cpd_benchmark",
-    version="0.1.4",
+    version="0.1.5",
     description="An open source benchmark for semi-supervised change point detection of type 1 diabetic meals from continuous glucose monitor time series data. Originally created to present to PyData Global 2024 in association with sktime and skchange.",
     url="https://github.com/Blood-Glucose-Control/t1d-change-point-detection-benchmark",
     packages=find_packages(),
--- t1d_cpd_benchmark/__init__.py
+++ t1d_cpd_benchmark/__init__.py
@@ -1,6 +1,3 @@
-from .datasets.raw_data import load_raw_data
-from .datasets.processed_data import load_processed_data
-from .datasets.obfuscated_data import load_obfuscated_data
 import os
 import shutil
 import urllib.request
